6718dd0ce7f4bc0b19fea855bd24c0652db1a72e
pukarlamichhane <pukarlamichhane567@gmail.com>
2024-02-07 22:27:39 +0545

    first commit

---
+++ navigation.js
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+const Stack = createNativeStackNavigator();
+import HomeScreen from './screens/HomeScreen';
+
+const Navigation = () => {
+  return (
+    <TailwindProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Discover" component={Discover} />
+          <Stack.Screen name="ItemScreen" component={ItemScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </TailwindProvider>
+  )
+}
+
+export default Navigation
---
+++ screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView className="bg-white ">
+        <Text>Go</Text>
+    </SafeAreaView>
+  )
+}
+
+export default HomeScreen
